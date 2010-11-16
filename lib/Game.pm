1cf15e6007b342c8e6ed20ab91eb407557cbf22b
kthakore <thakore.kartik@gmail.com>
2010-11-15 19:48:24 -0500

    Added the setup for the game clien

---
+++ lib/Game.pm
@@ -0,0 +1,40 @@
+package Game;
+use strict;
+use warnings;
+use SDL;
+use SDL::Video;
+use SDLx::App;
+
+
+sub new 
+{
+	my ($class) = shift;
+	my $self = {};
+	$self = bless($self, $class);
+
+	#Add an SDLx::App to hold
+
+	$self->{app} = SDLx::App->new( 
+		width => 700,
+		height => 700,
+		eoq => 1,
+		#We want double buffering
+		flags => SDL_HWSURFACE | SDL_DOUBLEBUF,
+		#We only want video initialized for now
+		init  => SDL_INIT_VIDEO,
+		#Title
+		title =>"Gravong Client"
+	);
+
+	return $self;
+
+}
+
+sub app :lvalue
+{
+	#Return the app of the first variable
+	# This will be $self if $self->app is called
+	$_[0]->{app};
+}
+
+1;
