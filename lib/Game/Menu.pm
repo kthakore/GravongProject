60844814caf8be1fb75a6afbccef158e0dff266c
kthakore <thakore.kartik@gmail.com>
2010-11-19 10:38:34 -0500

    Added a simple title screen. Need to move the menu

--- lib/Game/Menu.pm
+++ lib/Game/Menu.pm
@@ -1,42 +1,51 @@
 package Game::Menu;
 use strict;
 use warnings;
-use  SDLx::Widget::Menu;
+use SDLx::Widget::Menu;
 
-sub load{
+sub load {
 
-my ($class, $game) = @_;
+    my ( $class, $game ) = @_;
 
-my $self = bless({}, $class);
+    my $self = bless( {}, $class );
 
-$self->{menu} = SDLx::Widget::Menu->new(
-            font         => 'data/liberationserif-bold.ttf',
-            font_color   => [200, 200, 200],
-            select_color => [0, 0, 200],
-          #  change_sound => 'data/menu_select.ogg',
-    )->items(
-            'Create Game'    => sub { $self->{next} = 'new_game'   ; 0 },
-            'Join Game'   => sub { $self->{next} = 'load_game'  ; 0 },
-            'Quit'        => sub { return               },
+    $self->{title_screen} = SDLx::Surface->load('data/images/title.png');
+
+    $self->{menu} = SDLx::Widget::Menu->new(
+	    font         => 'data/liberationserif-bold.ttf',
+        font_color   => [ 200, 200, 200 ],
+        select_color => [ 0, 0, 200 ],
+
+        #  change_sound => 'data/menu_select.ogg',
+      )->items(
+        'Create Game' => sub { $self->{next} = 'new_game';  0 },
+        'Join Game'   => sub { $self->{next} = 'load_game'; 0 },
+        'Quit'        => sub { return },
+      );
+
+    my $app = $game->app();
+
+    my $e_handle =
+      $app->add_event_handler( sub { $self->{menu}->event_hook( $_[0] ) } );
+    my $m_handle = $app->add_move_handler( sub { } );
+    my $s_handle = $app->add_show_handler(
+        sub {
+            $_[1]->blit_by(
+                $self->{title_screen},
+                [ 0, 0, 480, 480 ],
+                [ 50, 0, 480, 480 ]
+            );
+            $self->{menu}->render( $_[1] );
+            $_[1]->update();
+        }
     );
 
-	my $app = $game->app();
+    $self->{handlers}->[0] = $m_handle;
+    $self->{handlers}->[1] = $s_handle;
+    $self->{handlers}->[2] = $e_handle;
 
-	my $e_handle = $app->add_event_handler( sub{ $self->{menu}->event_hook($_[0]) } );
-	my $m_handle = $app->add_move_handler( sub{  } );
-	my $s_handle = $app->add_show_handler( sub{ $self->{menu}->render($_[1]); $_[1]->update();  } );
-
-	 $self->{handlers}->[0] = $m_handle;
-	 $self->{handlers}->[1] = $s_handle;
-	 $self->{handlers}->[2] = $e_handle;
-
-
-return $self;
+    return $self;
 
 }
-
-
-
-
 
 1;
