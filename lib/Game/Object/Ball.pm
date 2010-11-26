0bd87bb30ca27a2bc1cfe6801f263af3f3d15202
kthakore <thakore.kartik@gmail.com>
2010-11-26 15:45:15 -0500

    Bouncing around

--- lib/Game/Object/Ball.pm
+++ lib/Game/Object/Ball.pm
@@ -29,26 +29,18 @@ sub interface : lvalue {
 sub _initialize {
     my ($self) = @_;
 
-    my $interface = $self->interface;
-    $interface->set_acceleration( sub { _acceleration( @_, $self->{level} ) } );
-
     my $app = $self->{app};
 
+
+    my $interface = $self->interface;
+    $interface->set_acceleration( sub { _acceleration( @_, $self->{level}, $app ) } );
     $interface->attach( $app, \&_show_ball, $app );
 
 }
 
-sub _gravity {
-	my ($x1, $x2, $size) = @_;
-
-	my $dist_square =  ($x1 - $x2 );
-
-	return ($size * 6)/ $dist_square ;
-
-}
 
 sub _acceleration {
-    my ( $time, $current_state, $level ) = @_;
+    my ( $time, $current_state, $level, $app ) = @_;
 
 	my $sum_accel_x = 0;
 	my $sum_accel_y = 0;
@@ -57,13 +49,38 @@ sub _acceleration {
 
 	foreach my $planet ( @planets )
 	{
-		
-		$sum_accel_x += _gravity( $current_state->x, $planet->{x}, $planet->{size} );
-		$sum_accel_y += _gravity( $current_state->y, $planet->{y}, $planet->{size} );
+		my $dx = ($planet->{x} - $current_state->x);
+		my $dy = ($planet->{y} -  $current_state->y);
+		my $D = sqrt( $dx**2 + $dy**2);
+		my $A = 7200 * $planet->{size} / $D ** 2 ;
 
+		if ($D < ($planet->{size} + 5 ) ) 
+		{
+			
+			
+			$sum_accel_x -= ($dx * $A/$D )*0.4;
+			$sum_accel_y -= ($dy * $A/$D )*0.4;
+
+		}
+		else
+		{
+			$sum_accel_x += $dx * $A/$D;
+			$sum_accel_y += $dy * $A/$D;
+		}
 
 	}
-    return ( $sum_accel_x, -$sum_accel_y , 0 );
+
+
+	if ( $current_state->x < 0 || $current_state->x > $app->w ) { $current_state->v_x( -$current_state->v_x ) };
+	if( $current_state->x < 0 ) { $current_state->x(0) }
+	$current_state->x( $app->w ) if $current_state->x > $app->w;
+
+	if ( $current_state->y < 0 || $current_state->y > $app->h ) { $current_state->v_y( -$current_state->v_y ) };
+
+	 $current_state->y(0) if $current_state->y < 0;
+	  $current_state->y( $app->h ) if $current_state->y > $app->h; 
+
+    return ( $sum_accel_x, $sum_accel_y  , 0 );
 }
 
 sub _show_ball {
