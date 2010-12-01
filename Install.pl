0741bf1ee81f880d018019073df32b82ea9bd1bc
kthakore <thakore.kartik@gmail.com>
2010-12-01 14:16:23 -0500

    Fixed install script

--- Install.pl
+++ Install.pl
@@ -1,8 +1,4 @@
 use Config;
-system ( split ' ', 'cpan YAML CPAN Alien::SDL pip Mouse Net::Address::IP::Local App::Packer::PAR');
+system ( split ' ', 'cpan YAML CPAN Alien::SDL pip Mouse Net::Address::IP::Local App::Packer::PAR SDL SDLx-Widget');
 
-system ( split ' ', 'pip SDL-2.523_2.tar.gz');
-
-system ( split ' ', 'pip SDLx-Widget-0.04.tar.gz');
-
-system ( split ' '. 'perl SDLpp.pl --output=gravong_'.$Config{archname}. ' --libs=SDL,SDL_main,SDL_gfx --input=gravong.pl --include=lib --more=Game,Clipboard::Xclip' );
+system ( split ' ', 'perl SDLpp.pl --output=gravong_'.$Config{archname}. ' --libs=SDL,SDL_main,SDL_gfx --input=gravong.pl --include=lib --more=Game,Clipboard::Xclip' );
